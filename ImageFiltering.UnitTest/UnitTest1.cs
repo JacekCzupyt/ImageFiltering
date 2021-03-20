4d1e774c6c6461b682c6af917081d2a7f73bae9b
JacekCzupyt <kuba.czupyt@gmail.com>
2021-03-20 19:36:49 +0100

    Started working on the octree color quantisation Also added testing project

---
+++ ImageFiltering.UnitTest/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using ImageFiltering.ImageProcessing;
+
+namespace ImageFiltering.UnitTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Oct
+        }
+    }
+}
---
+++ ImageFiltering/ImageProcessing/OctreeColorQuantisation.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ImageFiltering.ImageProcessing
+{
+    class OctreeColorQuantisation : ImageFilter
+    {
+        public string FilterType { get { return "Color Quantisers"; } }
+
+        public void Apply(Bitmap bitmap)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override string ToString()
+        {
+            return "Octree Color Quantiser";
+        }
+
+        class Octree
+        {
+            uint MaxLeafs, nLeafs = 0;
+            OctreeNode root;
+            SortedSet<OctreeNode> s = new SortedSet<OctreeNode>();
+
+            public Octree(uint MaxLeafs)
+            {
+                this.MaxLeafs = MaxLeafs;
+                
+            }
+        }
+
+        class OctreeNode : IComparable<OctreeNode>
+        {
+            int PixelCount = 0;
+            public int CompareTo(OctreeNode other)
+            {
+                return PixelCount - other.PixelCount;
+            }
+        }
+    }
+}
