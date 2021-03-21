1156dd3366a92bd067866135c0f77f8f0b7bd106
JacekCzupyt <kuba.czupyt@gmail.com>
2021-03-21 11:58:56 +0100

    Added arguably too many comments

--- ImageFiltering/ImageProcessing/OctreeColorQuantisation.cs
+++ ImageFiltering/ImageProcessing/OctreeColorQuantisation.cs
@@ -22,13 +22,16 @@ namespace ImageFiltering.ImageProcessing
             return "Octree Color Quantiser";
         }
 
+        /// <summary>
+        /// Octree used for color quantisation
+        /// </summary>
         class Octree
         {
             uint MaxLeafs, nLeafs = 0;
             OctreeNode root;
             List<OctreeNode>[] LeafsPerLayer = new List<OctreeNode>[8];
 
-            public Octree(uint MaxLeafs)
+            public Octree(uint MaxLeafs/*Maximum leafs allowed in the octree*/)
             {
                 this.MaxLeafs = MaxLeafs;
                 for(int i = 0; i < 8; i++)
@@ -37,8 +40,14 @@ namespace ImageFiltering.ImageProcessing
                 root = new OctreeNode(this, null, 0, new BitArray(24));
             }
 
-            ///Add a color of a pixel to the tree
-            public void AddColor(byte r, byte g, byte b, int count = 1)
+            /// <summary>
+            /// Adds a new color to the octree
+            /// </summary>
+            /// <param name="r">Red pixel value</param>
+            /// <param name="g">Green pixel value</param>
+            /// <param name="b">Blue pixel value</param>
+            /// <param name="count">Number of pixles to add of this color</param>
+            public void AddColor(byte r /*Red pixel value*/, byte g/*Green pixel value*/, byte b/*Blue pixel value*/, int count = 1/*Number of pixles to add of this color*/)
             {
                 //get leaf node of the tree
                 OctreeNode leaf = root.GetCorespondingLeaf(new BitArray(new byte[3] { r, g, b }));
@@ -56,7 +65,14 @@ namespace ImageFiltering.ImageProcessing
                 }
             }
 
-            public byte[] GetQuantisedColor(byte r, byte g, byte b)
+            /// <summary>
+            /// Returns the quantisation of the input color using the octree
+            /// </summary>
+            /// <param name="r">Red pixel value</param>
+            /// <param name="g">Green pixel value</param>
+            /// <param name="b">Blue pixel value</param>
+            /// <returns>Byte array representing the returned colors</returns>
+            public byte[] GetQuantisedColor(byte r /*Red pixel value*/, byte g/*Green pixel value*/, byte b/*Blue pixel value*/)
             {
                 //get leaf node of the tree
                 OctreeNode leaf = root.GetCorespondingLeaf(new BitArray(new byte[3] { r, g, b }));
@@ -76,16 +92,15 @@ namespace ImageFiltering.ImageProcessing
                 return res;
             }
 
-
-
-
-
-
+            /// <summary>
+            /// A single node of the octree
+            /// </summary>
             class OctreeNode
             {
                 Octree Tree;
                 OctreeNode Parent;
-                //if pixel count is null, it means that the node is not a leaf
+                
+                /// <summary>Number of pixels coresponding to this leaf, null if not leaf</summary>
                 public int? PixelCount;
                 public BitArray NodeId { get; private set; }
                 public int Layer { get; private set; }
@@ -99,6 +114,7 @@ namespace ImageFiltering.ImageProcessing
                     this.NodeId = NodeId;
                     this.PixelCount = PixelCount;
 
+
                     //ensure that any node on layer 8 is a leaf
                     if (Layer == 8 && PixelCount == null)
                     {
@@ -110,11 +126,21 @@ namespace ImageFiltering.ImageProcessing
                 }
 
                 
+                /// <summary>
+                /// Recursivly searches for the leaf corresponding to the provided color, creates it if it doesn't exist
+                /// </summary>
+                /// <param name="id">BitArray representing the rgb values of the pixel</param>
+                /// <returns>The leaf octree node coresponding to the provided color</returns>
                 public OctreeNode GetCorespondingLeaf(BitArray id)
                 {
+                    //if this is a leaf, return this
                     if (PixelCount.HasValue)
                         return this;
+
+                    //get index of child that should be entered
                     int index = 4 * (id[Layer] ? 1 : 0) + 2 * (id[8 + Layer] ? 1 : 0) + (id[16 + Layer] ? 1 : 0);
+
+                    //if child does not exist, create it
                     if (Children[index] == null)
                     {
                         BitArray newNodeId = NodeId;
@@ -123,9 +149,14 @@ namespace ImageFiltering.ImageProcessing
                         newNodeId[16 + Layer] = id[16 + Layer];
                         Children[index] = new OctreeNode(Tree, this, Layer + 1, newNodeId);
                     }
+
+                    //search recursivly
                     return Children[index].GetCorespondingLeaf(id);
                 }
 
+                /// <summary>
+                /// Reduces this node by calling the gather method of its parent
+                /// </summary>
                 public void Reduce()
                 {
                     if (Parent == null)
@@ -133,18 +164,25 @@ namespace ImageFiltering.ImageProcessing
                     Parent.Gather();
                 }
 
+                /// <summary>
+                /// Recursivly gathers the pixel counts of all its children and becomes a leaf
+                /// </summary>
                 private void Gather()
                 {
+                    //if is leaf, do nothing
                     if (PixelCount.HasValue)
                         return;
 
                     PixelCount = 0;
 
+                    //for each child
                     for(int i=0;i<8;i++)
                     {
                         var Node = Children[i];
+                        //if child exists
                         if (Node != null)
                         {
+                            //recursivly gather it, collect its pixel count and remove if from children and LeafsPerLayer array
                             Node.Gather();
                             PixelCount += Node.PixelCount;
                             Tree.LeafsPerLayer[Node.Layer].Remove(Node);
@@ -153,6 +191,7 @@ namespace ImageFiltering.ImageProcessing
                         }
                     }
 
+                    //mark this as leaf
                     Tree.nLeafs++;
                     Tree.LeafsPerLayer[Layer].Add(this);
                 }
